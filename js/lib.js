4c8cffb9ac55a8885238ea1ac1121f1405612778
nasim-saaidi <302263973@student.rocmondriaan.nl>
2022-11-24 09:34:31 +0100

    big update

--- js/lib.js
+++ js/lib.js
@@ -1,16 +1,95 @@
-let click1 = false
+let turn = 'x'
+const buttons = document.querySelectorAll(".wrapper > div");
+let arr = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 
+// buttons.forEach(function (element) {
+//     element.addEventListener("click", () => {
+//         element.classList.add('x-filled');
+//         click1 = true;
+//         console.log("bruh");
+//     });
+// })
 
-const x = document.querySelector(".x");
+// while (click1 == true) {
 
-function myFunction() {
-    x.style.display = "block";
-    click = true;
-    console.log("click")
+//     buttons.forEach(function (element) {
+//         element.addEventListener("click", () => {
+//             element.classList.add('y-filled');
+//             console.log("click");
+//             click1 = false;
+//             console.log("hurb");
+//         });
+//     })
+// }
+
+function switchTurn() {
+
 }
 
-const buttons = document.querySelectorAll(".wrapper > div");
+function updateArray(element, index) {
+    if (element.classList.contains("x-filled")) {
+        arr[index] = 1;
+    } else if (element.classList.contains("y-filled")) {
+        arr[index] = -1;
+    }
 
-buttons.forEach(function(element) {
-    element.addEventListener("click", myFunction);
-})
+}
+
+function vibecheck() {
+    if (arr[0] + arr[3] + arr[6] == 3) {
+        alert('x won');
+    }
+    else if (arr[0] + arr[3] + arr[6] == -3) {
+        alert('y won');
+    }
+    else if (arr[1] + arr[4] + arr[7] == 3) {
+        alert('x won');
+    }
+    else if (arr[1] + arr[4] + arr[7] == -3) {
+        alert('Y won');
+    }
+    else if (arr[2] + arr[5] + arr[8] == 3) {
+        alert('x won');
+    }
+    else if (arr[2] + arr[5] + arr[8] == -3) {
+        alert('Y won');
+    }
+    else if (arr[0] + arr[1] + arr[3] == 3) {
+        alert('X won')
+    }
+    else if (arr[0] + arr[1] + arr[3] == -3) {
+        alert('Y won')
+    }
+    else if (arr[3] + arr[4] + arr[5] == 3) {
+        alert('X won')
+    }
+    else if (arr[3] + arr[4] + arr[5] == -3) {
+        alert('Y won')
+    }
+    else if(arr[6] + arr[7] + arr[8] == 3) {
+        alert('X won')
+    }
+    else if(arr[6] + arr[7] + arr[8] == -3) {
+        alert("Y won")
+        
+    }
+}
+buttons.forEach((element, index) => {
+    buttons[index].addEventListener("click", () => {
+        switch (turn) {
+            case "x":
+                element.classList.add('x-filled');
+                updateArray(element, index);
+                vibecheck();
+                turn = 'y';
+                break;
+            case "y":
+                element.classList.add('y-filled');
+                updateArray(element, index);
+                vibecheck();
+                turn = "x"
+                break;
+        }
+    })
+});
+console.log(arr)
