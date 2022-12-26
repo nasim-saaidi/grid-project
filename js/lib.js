4ff6cdaf172b7f40704885330c1f8fee214a0ef7
nasim-saaidi <302263973@student.rocmondriaan.nl>
2022-12-26 14:06:55 +0100

    localstorage aan werken

--- js/lib.js
+++ js/lib.js
@@ -8,21 +8,37 @@ function updateArray(element, index) {
 }
 
 function XWinConfirm() {
+    pointsPlayerX = parseInt(localStorage.getItem("Xpoints"));
+   
     pointsPlayerX++;
+    localStorage.setItem("Xpoints", pointsPlayerX);
+    console.log(pointsPlayerX);
+    console.log(parseInt(localStorage.getItem("Xpoints")));
     pointsX.textContent = pointsPlayerX;
-    alert("x won")
-    gameOver = true
+    alert("x won");
+    gameOver = true;
     
+
 }
 
 function YWinConfirm() {
-    pointsPlayerO++;
+    pointsPlayerO = parseInt(localStorage.getItem("Opoints"));
+    pointsPlayerO++
+    localStorage.setItem("Opoints", pointsPlayerO);
+    console.log(pointsPlayerO);
+    console.log(parseInt(localStorage.getItem("Opoints")))
     pointsy.textContent = pointsPlayerO;
     alert('y won');
-        gameOver = true;
+    gameOver = true;
+
 }
 
 function botPressO() {
-    Math.round(Math.random() * arr.length);
-    element.classList.add('y-filled');
+    for (let i = -1; i < arr.length; i++) {
+        const botMaths = arr[i];
+        console.log(botMaths)
+        // if(arr.contains(0)) {
+        // element.classList.add('y-filled')    
+        // }
+    }
 }
--- js/main.js
+++ js/main.js
@@ -7,157 +7,115 @@ const points1 = pointsX.textContent;
 const pointsy = document.querySelector(".pointO");
 const points2 = pointsy.textContent;
 let pvp = true;
-let pointsPlayerX = 0;
-let pointsPlayerO = 0;
+let pointsPlayerX = parseInt(localStorage.getItem("Xpoints"));
+pointsX.textContent = pointsPlayerX;
+let pointsPlayerO = parseInt(localStorage.getItem("Opoints"));
+pointsy.textContent = pointsPlayerO;
 firstturn = true;
 
-// buttons.forEach(function (element) {
-//     element.addEventListener("click", () => {
-//         element.classList.add('x-filled');
-//         click1 = true;
-//         console.log("bruh");
-//     });
-// })
+    
 
-// while (click1 == true) {
-
-//     buttons.forEach(function (element) {
-//         element.addEventListener("click", () => {
-//             element.classList.add('y-filled');
-//             console.log("click");
-//             click1 = false;
-//             console.log("hurb");
-//         });
-//     })
-// }
-
-
-
-
-// function Haltx() {
-//     element.classList.add('x-filled');
-//     updateArray(element, index);
-//     vibecheck();
-// }
-
-// function Halty() {
-//     element.classList.add('Y-filled');
-//     updateArray(element, index);
-//     vibecheck();
-// }
 
 
 function winCheck() {
     if (arr[0] + arr[3] + arr[6] == 3) {
         XWinConfirm();
+        
+        
     }
     else if (arr[0] + arr[3] + arr[6] == -3) {
-        YWinConfirm() 
+        YWinConfirm();
+        
     }
     else if (arr[1] + arr[4] + arr[7] == 3) {
-        XWinConfirm()
+        XWinConfirm();
+        
     }
     else if (arr[1] + arr[4] + arr[7] == -3) {
-        YWinConfirm()
+        YWinConfirm();
+        
     }
     else if (arr[2] + arr[5] + arr[8] == 3) {
-        XWinConfirm()
+        XWinConfirm();
+        
     }
     else if (arr[2] + arr[5] + arr[8] == -3) {
-        YWinConfirm()
+        YWinConfirm();
+        
     }
     else if (arr[0] + arr[1] + arr[2] == 3) {
-        XWinConfirm()
+        XWinConfirm();
     }
     else if (arr[0] + arr[1] + arr[2] == -3) {
-        YWinConfirm()
+        YWinConfirm();
     }
     else if (arr[3] + arr[4] + arr[5] == 3) {
-        XWinConfirm()
+        XWinConfirm();
     }
     else if (arr[3] + arr[4] + arr[5] == -3) {
-        YWinConfirm()
+        YWinConfirm();
     }
     else if (arr[6] + arr[7] + arr[8] == 3) {
-        XWinConfirm()
+        XWinConfirm();
     }
     else if (arr[6] + arr[7] + arr[8] == -3) {
-        YWinConfirm()
+        YWinConfirm();
+        
     }
     else if (arr[0] + arr[4] + arr[8] == 3) {
         XWinConfirm()
-
     }
     else if (arr[0] + arr[4] + arr[8] == -3) {
-        YWinConfirm()
+        YWinConfirm();
     }
     else if (arr[6] + arr[4] + arr[2] == 3) {
-        XWinConfirm()
-
+        XWinConfirm();
     }
     else if (arr[6] + arr[4] + arr[2] == -3) {
-        YWinConfirm()
+        YWinConfirm();
     }
 
 }
 
-const myBtn = document.querySelector('.reset');
 
 
-myBtn.addEventListener('click', function () {
+const reset = document.querySelector('.reset');
+
+
+reset.addEventListener('click', function () {
 
     arr = [0, 0, 0, 0, 0, 0, 0, 0, 0];
-    buttons.forEach(function(element) {
-        element.classList = null;})
+    buttons.forEach(function (element) {
+        element.classList = null;
+
+    })
+    localStorage.setItem("Xpoints", "0");
+    localStorage.setItem("Opoints", "0");
+    console.log(localStorage);
     gameOver = false;
+    pointsX.textContent = pointsPlayerX;
+    pointsy.textContent = pointsPlayerO;
 });
 
-if (pvp == true ) {
-buttons.forEach((element, index) => {
-    buttons[index].addEventListener("click", () => {
-        if (arr[index] == 0) {
-            if (gameOver == false) {
-
-                switch (turn) {
-                    case "x":
-                        element.classList.add('x-filled');
-                        updateArray(element, index);
-                        winCheck();
-                        turn = 'y';
-                        break;
-                        x = false
-
-                    case "y":
-                        element.classList.add('y-filled');
-                        updateArray(element, index);
-                        winCheck();
-                        turn = "x"
-                        break;
-                }
-            }
-        }
-    })
-})
-}
-
-if (pvp == false ) {
+if (pvp == true) {
     buttons.forEach((element, index) => {
         buttons[index].addEventListener("click", () => {
             if (arr[index] == 0) {
                 if (gameOver == false) {
+
                     switch (turn) {
                         case "x":
                             element.classList.add('x-filled');
                             updateArray(element, index);
-                            vibecheck();
+                            winCheck();
                             turn = 'y';
                             break;
                             x = false
-    
+
                         case "y":
                             element.classList.add('y-filled');
                             updateArray(element, index);
-                            vibecheck();
+                            winCheck();
                             turn = "x"
                             break;
                     }
@@ -165,19 +123,46 @@ if (pvp == false ) {
             }
         })
     })
-    }
+}
+
+// if (pvp == false) {
+//     buttons.forEach((element, index) => {
+//         buttons[index].addEventListener("click", () => {
+//             if (arr[index] == 0) {
+//                 if (gameOver == false) {
+//                     switch (turn) {
+//                         case "x":
+//                             element.classList.add('x-filled');
+//                             updateArray(element, index);
+//                             vibecheck();
+//                             turn = 'y';
+//                             break;
+//                             x = false
+
+//                         case "y":
+//                             element.classList.add('y-filled');
+//                             botPressO
+//                             updateArray(element, index);
+//                             winCheck();
+//                             turn = "x"
+//                             break;
+//                     }
+//                 }
+//             }
+//         })
+//     })
+// }
+
+console.log(arr)
 
 let username1 = document.querySelector(".username1");
 
 
-username1.onkeydown = function(e) {
-   localStorage.setItem('.name1', 'ye').value
-    alert(localStorage.getItem('name1'))   
+username1.onkeydown = function (e) {
+//    localStorage.setItem('.name1', 'ye')
+    alert(localStorage.getItem('name1'))
+}
 
-    
-} 
-
-console.log(player1)
 
 
 
