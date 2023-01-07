0691ef9c827a6a7dda35f247734b08c7cc5188e8
nasim-saaidi <302263973@student.rocmondriaan.nl>
2023-01-07 18:03:50 +0100

    pve bug fixing

--- js/main.js
+++ js/main.js
@@ -7,23 +7,30 @@ const pointsX = document.querySelector(".pointX");
 const points1 = pointsX.textContent;
 const pointsY = document.querySelector(".pointO");
 const points2 = pointsY.textContent;
-let pvp = true;
-console.log(pvp)
+let pvp;
 let pointsPlayerX = parseInt(localStorage.getItem("Xpoints"));
 pointsX.textContent = pointsPlayerX;
 let pointsPlayerO = parseInt(localStorage.getItem("Opoints"));
 pointsY.textContent = pointsPlayerO;
+let tracker = 0;
+lastMove();
 
 
+const bot = document.querySelector('.pve');
+bot.addEventListener('click', activatePve);
 
-const playerVplayer = document.querySelector('.pve');
-playerVplayer.addEventListener('click', work);
-
-function work() {
-      pvp = false;
-     console.log(pvp)
+function activatePve() {
+    pvp = false;
+    movementCheck()
 }
 
+const regular = document.querySelector('.pvp');
+regular.addEventListener('click', activatePvp);
+
+function activatePvp() {
+    pvp = true;
+    movementCheck()
+}
 
 function winCheck() {
     if (arr[0] + arr[3] + arr[6] == 3) {
@@ -113,58 +120,85 @@ reset.addEventListener('click', function () {
     localStorage.setItem("Opoints", "0");
 });
 
+function movementCheck() {
+    if (pvp == true) {
+        buttons.forEach((element, index) => {
+            buttons[index].addEventListener("click", () => {
+                if (arr[index] == 0) {
+                    if (gameOver == false) {
 
-function bot() {
-    buttons.forEach((element, index) => {
-        buttons[index].addEventListener("click", () => {
-            if (arr[index] == 0) {
-                if (gameOver == false) {
-                    element.classList.add('x-filled');
-                    updateArray(element, index);
-                    turn = 'y';
-                    botPressO();
-                    winCheck();
+                        switch (turn) {
+                            case "x":
+                                element.classList.add('x-filled');
+                                updateArray(element, index);
+                                winCheck();
+                                turn = 'y';
+                                break;
+                                x = false
 
-                }
-            }
-        })
-    })
-}
-
-if (pvp == true) {
-     regular()
-}
-else if (pvp == false ) {
-     bot()
-}
-
-function regular() {
-    buttons.forEach((element, index) => {
-        buttons[index].addEventListener("click", () => {
-            if (arr[index] == 0) {
-                if (gameOver == false) {
-
-                    switch (turn) {
-                        case "x":
-                            element.classList.add('x-filled');
-                            updateArray(element, index);
-                            winCheck();
-                            turn = 'y';
-                            break;
-                            x = false
-
-                        case "y":
-                            element.classList.add('y-filled')
-                            updateArray(element, index);
-                            winCheck();
-                            turn = "x";
-                            break;
+                            case "y":
+                                element.classList.add('y-filled')
+                                updateArray(element, index);
+                                winCheck();
+                                turn = "x";
+                                break;
+                        }
                     }
                 }
-            }
+            })
+        });
+    }
+    else if (pvp == false) {
+        buttons.forEach((element, index) => {
+            buttons[index].addEventListener("click", () => {
+                if (arr[index] == 0) {
+                    if (gameOver == false && tracker < 4) {
+                        element.classList.add('x-filled');
+                        updateArray(element, index);
+                        turn = 'y';
+                        botPressO();
+                        winCheck();
+                        tracker++;
+                        lastMove();
+                    }
+                }
+            })
         })
-    });
+    }
 }
+
+
+function lastMove() {
+    if (tracker >= 4 && arr.contain == '0') {
+        buttons.forEach((element, index) => {
+            buttons[index].addEventListener("click", () => {
+                if (arr[index] == 0) {
+                    if (gameOver == false) {
+
+                        switch (turn) {
+                            case "x":
+                                element.classList.add('x-filled');
+                                updateArray(element, index);
+                                winCheck();
+                                turn = 'y';
+                                break;
+                                x = false
+
+                            case "y":
+                                element.classList.add('y-filled')
+                                updateArray(element, index);
+                                winCheck();
+                                turn = "x";
+                                break;
+                        }
+                    }
+                }
+            })
+        });
+    }
+    tracker = 0
+}
+
 console.log(arr)
 
 
