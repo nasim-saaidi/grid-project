455b4d49f5859599d4141f523ac1289923268ade
nasim-saaidi <302263973@student.rocmondriaan.nl>
2023-01-20 13:38:00 +0100

    final update

--- js/lib.js
+++ js/lib.js
@@ -1,18 +1,15 @@
 function updateArray(element, index) {
     if (element.classList.contains("x-filled")) {
         arr[index] = 1;
-    } else if (element.classList.contains("y-filled")) {
+    } else if (element.classList.contains("o-filled")) {
         arr[index] = -1;
     }
-
 }
 
 function xWinConfirm() {
     pointsPlayerX = parseInt(localStorage.getItem("Xpoints"));
     pointsPlayerX++;
     localStorage.setItem("Xpoints", pointsPlayerX);
-    console.log(pointsPlayerX);
-    console.log(parseInt(localStorage.getItem("Xpoints")));
     pointsX.textContent = pointsPlayerX;
     if (window.localStorage.getItem('playerOneName') == '') {
         alert("player X won")
@@ -25,29 +22,26 @@ function xWinConfirm() {
 
 function yWinConfirm() {
     pointsPlayerO = parseInt(localStorage.getItem("Opoints"));
-    pointsPlayerO++
+    pointsPlayerO++;
     localStorage.setItem("Opoints", pointsPlayerO);
-    console.log(pointsPlayerO);
-    console.log(parseInt(localStorage.getItem("Opoints")))
     pointsY.textContent = pointsPlayerO;
     if (window.localStorage.getItem('playerTwoName') == '') {
-        alert("player Y won")
+        alert("player O won")
     }
     else {
         alert(window.localStorage.getItem('playerTwoName') + ' won')
     };
     gameOver = true;
-
 }
 
-const save = document.querySelector('.saveName');
+const save = document.querySelector('.save-name');
+
 save.addEventListener('click', myFunction);
 
 function myFunction() {
     window.localStorage.setItem('playerOneName', input.value);
     window.localStorage.setItem('playerTwoName', input2.value);
 }
-
 
 function generateRandomNumber() {
     return Math.round(Math.random() * 9);
@@ -60,12 +54,13 @@ function botPressO() {
     let randomMove = -1;
     while (arr[randomMove] != 0 && arr.includes(0)) {
         randomMove = generateRandomNumber();
-    };
+    }
     arr[randomMove] = -1;
-    buttons[randomMove].classList.add("y-filled");
+    buttons[randomMove].classList.add("o-filled");
 }
 
 const bot = document.querySelector('.pve');
+
 bot.addEventListener('click', activatePve);
 
 function activatePve() {
@@ -74,6 +69,7 @@ function activatePve() {
 }
 
 const regular = document.querySelector('.pvp');
+
 regular.addEventListener('click', activatePvp);
 
 function activatePvp() {
@@ -82,79 +78,75 @@ function activatePvp() {
 }
 
 function tieChecker() {
-    console.log(!arr.includes(0))
     if (tie != false && !arr.includes(0)) {
-        alert("you tied")
-    };
+        alert("you tied");
+    }
 }
 
 function winCheck() {
     if (arr[0] + arr[3] + arr[6] == 3) {
         xWinConfirm();
-        tie = false
-
+        tie = false;
     }
     else if (arr[0] + arr[3] + arr[6] == -3) {
         yWinConfirm();
-        tie = false
-
+        tie = false;
     }
     else if (arr[1] + arr[4] + arr[7] == 3) {
         xWinConfirm();
-        tie = false
+        tie = false;
     }
     else if (arr[1] + arr[4] + arr[7] == -3) {
         yWinConfirm();
-        tie = false
+        tie = false;
     }
     else if (arr[2] + arr[5] + arr[8] == 3) {
         xWinConfirm();
-        tie = false
+        tie = false;
     }
     else if (arr[2] + arr[5] + arr[8] == -3) {
         yWinConfirm();
-        tie = false
+        tie = false;
     }
     else if (arr[0] + arr[1] + arr[2] == 3) {
         xWinConfirm();
-        tie = false
+        tie = false;
     }
     else if (arr[0] + arr[1] + arr[2] == -3) {
         yWinConfirm();
-        tie = false
+        tie = false;
     }
     else if (arr[3] + arr[4] + arr[5] == 3) {
         xWinConfirm();
-        tie = false
+        tie = false;
     }
     else if (arr[3] + arr[4] + arr[5] == -3) {
         yWinConfirm();
-        tie = false
+        tie = false;
     }
     else if (arr[6] + arr[7] + arr[8] == 3) {
         xWinConfirm();
-        tie = false
+        tie = false;
     }
     else if (arr[6] + arr[7] + arr[8] == -3) {
         yWinConfirm();
-        tie = false
-
+        tie = false;
     }
     else if (arr[0] + arr[4] + arr[8] == 3) {
         xWinConfirm()
-        tie = false
+        tie = false;
     }
     else if (arr[0] + arr[4] + arr[8] == -3) {
         yWinConfirm();
-        tie = false
+        tie = false;
     }
     else if (arr[6] + arr[4] + arr[2] == 3) {
         xWinConfirm();
-        tie = false
+        tie = false;
     }
     else if (arr[6] + arr[4] + arr[2] == -3) {
         yWinConfirm();
-        tie = false
+        tie = false;
     }
 }
 
@@ -164,7 +156,6 @@ function movementCheck() {
             buttons[index].addEventListener("click", () => {
                 if (arr[index] == 0) {
                     if (gameOver == false) {
-
                         switch (turn) {
                             case "x":
                                 element.classList.add('x-filled');
@@ -173,9 +164,8 @@ function movementCheck() {
                                 tieChecker();
                                 turn = 'y';
                                 break;
-
                             case "y":
-                                element.classList.add('y-filled')
+                                element.classList.add('o-filled')
                                 updateArray(element, index);
                                 winCheck();
                                 tieChecker();
@@ -203,4 +193,4 @@ function movementCheck() {
             })
         });
     }
-};
+}
--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
-let turn = 'x'
+let turn = 'x';
 const buttons = document.querySelectorAll(".box");
 let arr = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 let gameOver = false;
-const pointsX = document.querySelector(".pointX");
+const pointsX = document.querySelector(".point-x");
 const points1 = pointsX.textContent;
-const pointsY = document.querySelector(".pointO");
+const pointsY = document.querySelector(".point-O");
 const points2 = pointsY.textContent;
 let pvp;
 let tie;
@@ -12,36 +12,32 @@ let pointsPlayerX = parseInt(localStorage.getItem("Xpoints"));
 pointsX.textContent = pointsPlayerX;
 let pointsPlayerO = parseInt(localStorage.getItem("Opoints"));
 pointsY.textContent = pointsPlayerO;
-
+let input = document.querySelector('.input');
+let input2 = document.querySelector('.input2');
+let playerOneName;
+let playerTwoName;
 const reload = document.querySelector('.reload');
+const reset = document.querySelector('.reset');
+console.log(arr);
 
 reload.addEventListener('click', function () {
     location.reload();
     movementCheck();
 })
 
-const reset = document.querySelector('.reset');
-
 reset.addEventListener('click', function () {
     location.reload();
-    console.log(localStorage);
     gameOver = false;
-    let resetO = 0
-    let resetX = 0
-    pointsY.textContent = resetO
-    pointsX.textContent = resetX
+    let resetO = 0;
+    let resetX = 0;
+    pointsY.textContent = resetO;
+    pointsX.textContent = resetX;
     localStorage.setItem("Xpoints", "0");
     localStorage.setItem("Opoints", "0");
 })
 
-console.log(arr)
 
-let input = document.querySelector('.input');
 
-let input2 = document.querySelector('.input2')
-
-let playerOneName;
-let playerTwoName;
 
 
 
